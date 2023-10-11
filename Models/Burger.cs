5d3121d7f9ed00a2a54638b44d23e38337a0fd29
laboratorio <laboratorio@UPOAULA10620>
2023-10-11 08:44:05 -0500

    Burger Creado

---
+++ Models/Burger.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArielRamos_Taller.Models
+{
+    public class Burger
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public bool WithCheese { get; set; }
+        [Range(0.01, 100.99)]
+        public decimal Precio { get; set; }
+    }
+}
